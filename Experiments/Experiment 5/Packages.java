5f6625669a346e65a548e14de007e510baba7293
Sumit Patel <136911724+SumitPatel-HQ@users.noreply.github.com>
2024-09-12 14:57:36 +0530

    Create Packages.java

---
+++ Experiments/Experiment 5/Packages.java
@@ -0,0 +1,16 @@
+public class Test 
+{
+
+public static void main(String args[])
+
+{
+
+double number = 144.0;
+
+double squareRoot = Math.sqrt(number);
+
+System.out.println("\n\n\tThe square root of " + number + " is: " + squareRoot);
+
+}
+
+}
